7e7e09d00c4331f4f7e9833cecd3920beed9dc43
Tanya <tanyasingla89@gmail.com>
2026-06-29 17:33:26 +0530

    Create 1. 2Sum.cpp

---
+++ Arrays/1. 2Sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            int complement=target-nums[i];
+            if(mp.count(complement)>0) return {i,mp[complement]};
+            mp[nums[i]]=i;
+        }
+return { -1,-1};
+    }
+};
