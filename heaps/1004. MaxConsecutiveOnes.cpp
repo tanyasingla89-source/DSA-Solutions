bdaa9ed7efe45388468e5b3ab58d109da4eefc47
Tanya <tanyasingla89@gmail.com>
2026-06-30 20:40:56 +0530

    Create 1004. MaxConsecutiveOnes.cpp

---
+++ heaps/1004. MaxConsecutiveOnes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l=0;
+        int r=0;
+        int noz=0;
+        int maxlen=0;
+        while(r<nums.size())
+        {
+            if(nums[r]==0)
+            {
+            noz++;
+            while(noz>k)
+            {
+                if(nums[l]==0)
+                {
+                    noz--;
+                }
+                l++;
+            }
+            }
+        
+        maxlen=max(maxlen,r-l+1);
+        r++;}
+        return maxlen;
+    }
+};
