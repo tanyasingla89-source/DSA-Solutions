06d92c736fb1d6addcff5a552b644f91c54e3259
Tanya <tanyasingla89@gmail.com>
2026-07-01 12:02:05 +0530

    Create preOrderThroughDStack.cpp

---
+++ BinaryTrees/preOrderThroughDStack.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(root==NULL) return ans;
+        stack<TreeNode*>s;
+        s.push(root);
+        while(!s.empty())
+        {
+            auto top=s.top();
+            s.pop();
+            ans.push_back(top->val);
+            if(top->right!=NULL) s.push(top->right);
+            if(top->left!=NULL) s.push(top->left);
+        }
+        return ans;
+
+    }
+};
