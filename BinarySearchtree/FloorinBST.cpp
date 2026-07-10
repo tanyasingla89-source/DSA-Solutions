08c36718f7be511edd473346f401f8f9c5eac3cd
Tanya <tanyasingla89@gmail.com>
2026-07-10 05:47:05 +0530

    Create FloorinBST.cpp

---
+++ BinarySearchtree/FloorinBST.cpp
@@ -0,0 +1,43 @@
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+    int findMaxFork(Node* root, int x) {
+        if(!root) return -1;
+        Node* curr=root;
+        int ceil=-1;
+        while(curr)
+        {
+            if(curr->data>x)
+            {
+                curr=curr->left;
+            }
+            else if(curr->data<x)
+            {
+                ceil=curr->data;
+                curr=curr->right;
+            }else
+            {
+                ceil=curr->data;
+                break;
+            }
+        }
+        return ceil;
+        
+    }
+};
+
+        
+ 
