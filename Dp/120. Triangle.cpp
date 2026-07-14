a0a1c32d5e209c10987484d1ef0633af18fe9639
Tanya <tanyasingla89@gmail.com>
2026-07-14 11:30:53 +0530

    Create 120. Triangle.cpp

---
+++ Dp/120. Triangle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+        vector<int> dp(triangle[m-1]);  // start with last row values
+
+        for (int i = m-2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j+1]);
+            }
+        }
+        return dp[0];
+    }
+};
